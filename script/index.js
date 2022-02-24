0fe329f76cbb91db3610fc64a2e2e6f352176f82
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-02-25 00:34:44 +0300

    feat: реализовать функциональность попапа

---
+++ script/index.js
@@ -0,0 +1,31 @@
+
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__username');
+let aboutInput = document.querySelector('.form__about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
+let addButton = document.querySelector('.profile__add-button');
+let editButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+
+function popupCloseHandler() {
+    popup.classList.remove('popup_opened');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
+    popupCloseHandler();
+}
+
+function popupOpenHandler() {
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileAbout.textContent;
+}
+
+editButton.addEventListener('click', popupOpenHandler);
+formElement.addEventListener('submit', formSubmitHandler);
+popupCloseButton.addEventListener('click', popupCloseHandler);
