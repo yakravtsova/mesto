5423a98e62d45e70c69de04e58dac40917e7d16a
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-02-25 15:06:22 +0300

    refactor: некоторые стили и свойства приведены в соответствие с брифом

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 
-let formElement = document.querySelector('.form');
-let nameInput = document.querySelector('.form__username');
-let aboutInput = document.querySelector('.form__about');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup_input_username');
+let aboutInput = document.querySelector('.popup_input_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let addButton = document.querySelector('.profile__add-button');
