a562eb2f49b4f0435b4891ada1a27c575479e981
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-03-14 23:32:51 +0300

    fix: исправлены названия функций, обработчики навешены при создании карточки

--- script/index.js
+++ script/index.js
@@ -16,7 +16,8 @@ const viewPopup = document.querySelector('.popup_feature_view');
 const viewCloseButton = document.querySelector('.popup__close-button_feature_view');
 const elementTemplate = document.querySelector('.element__template').content;
 const elementsList = document.querySelector('.elements__list');
-const removeButton = document.querySelector('.element__remove-button');
+const popupViewImage = document.querySelector('.popup__view-image');
+const popupImageCaption = document.querySelector('.popup__image-caption');
 const initialCards = [
     {
       name: 'Архыз',
@@ -47,9 +48,21 @@ const initialCards = [
 function renderElement(card) {
   //функция возвращает одну новую карточку по шаблону, но не выводит её на страницу
   const element = elementTemplate.querySelector('.element').cloneNode(true);
-  element.querySelector('.element__image').src = card.link;
+  const elementImage = element.querySelector('.element__image');
+  elementImage.src = card.link;
+  elementImage.alt = card.name;
   element.querySelector('.element__title').textContent = card.name;
-  element.querySelector('.element__image').alt = card.name;
+  //удаление карточки
+  element.querySelector('.element__remove-button').addEventListener('click', () => element.remove());
+  //лайк
+  element.querySelector('.element__like-button').addEventListener('click', (evt) => evt.target.classList.toggle('element__like-button_active'));
+  //просмотр фотографии
+  elementImage.addEventListener('click', (evt) => {
+    openPopup(viewPopup);
+    popupViewImage.src = evt.target.src;
+    popupViewImage.alt = evt.target.alt;
+    popupImageCaption.textContent = evt.target.alt;
+  })
   return element;
 }
 //создадим массив карточек
@@ -57,73 +70,50 @@ const elementsArray = initialCards.map(renderElement);
 //размещаем исходные карточки на странице
 elementsArray.forEach((item) => elementsList.append(item));
 
-function openPopupHandler(popup) {
+function openPopup(popup) {
     popup.classList.add('popup_opened');
 }
 
-function closePopupHandler(popup) {
+function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function editFormSubmitHandler(evt) {
+function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
-    closePopupHandler(editPopup);
+    closePopup(editPopup);
 }
 
-function addFormSubmitHandler(evt) {
+function handleAddingFormSubmit(evt) {
   evt.preventDefault();
   //создаём объект с названием достопримечательности и ссылкой на картинку
-  let place = {};
-  place.name = addPlace.value;
-  place.link = addImageLink.value;
+  const place = {
+    name: addPlace.value,
+    link: addImageLink.value
+  };
   //создаём и сразу выводим новую карточку на страницу в начало списка карточек
   elementsList.prepend(renderElement(place));
-  closePopupHandler(addPopup);
+  closePopup(addPopup);
+  //очищаем форму
+  addFormElement.reset();
 }
 
 editButton.addEventListener('click', function() {
-    openPopupHandler(editPopup);
+    openPopup(editPopup);
     //при открытии попапа редактирования профиля поля формы заняты исходными данными пользователя
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
 });
 
-addButton.addEventListener('click', () => {
-  openPopupHandler(addPopup);
-  //при открытии попапа добавления карточки поля формы пустые
-  addPlace.value = '';
-  addImageLink.value = '';
-});
+addButton.addEventListener('click', () => openPopup(addPopup));
 
-editCloseButton.addEventListener('click', () => closePopupHandler(editPopup));
-addCloseButton.addEventListener('click', () => closePopupHandler(addPopup));
-viewCloseButton.addEventListener('click', () => closePopupHandler(viewPopup));
+editCloseButton.addEventListener('click', () => closePopup(editPopup));
+addCloseButton.addEventListener('click', () => closePopup(addPopup));
+viewCloseButton.addEventListener('click', () => closePopup(viewPopup));
 
-editFormElement.addEventListener('submit', editFormSubmitHandler);
-addFormElement.addEventListener('submit', addFormSubmitHandler);
-//обработчик событий для контейнера с карточками
-document.querySelector('.elements').addEventListener('click', (evt) => {
-  const element = evt.target.closest('.element');
-  if (!element) {
-    return;
-  }
-  //удаление карточки
-  if (evt.target.classList.contains('element__remove-button')) {
-    element.remove();
-  }
-  //лайк
-  else if (evt.target.classList.contains('element__like-button')) {
-    evt.target.classList.toggle('element__like-button_active');
-  }
-  //просмотр фотографии
-  else if (evt.target.classList.contains('element__image')) {
-    openPopupHandler(viewPopup);
-    document.querySelector('.popup__view-image').src = element.querySelector('.element__image').src;
-    document.querySelector('.popup__view-image').alt = element.querySelector('.element__title').textContent;
-    document.querySelector('.popup__image-caption').textContent = element.querySelector('.element__title').textContent;
-  }
-});
+editFormElement.addEventListener('submit', handleProfileFormSubmit);
+addFormElement.addEventListener('submit', handleAddingFormSubmit);
+
 
   
