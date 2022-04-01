eafb9fcd68bedd52bedb123e0f2694aafca8a58f
Margarita Kravtsova <margaritaselez@yandex.ru>
2022-04-01 15:44:15 +0300

    feat: прокомментировала

--- script/validate.js
+++ script/validate.js
@@ -1,15 +1,20 @@
 const showInputError = (formElement, inputElement, errorMessage, inputErrorClass) => {
+  //нашли поле с ошибкой
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  //добавили класс ошибки и вывели сообщение
   inputElement.classList.add(inputErrorClass);
   errorElement.textContent = errorMessage;
 }
 
 const hideInputError = (formElement, inputElement, inputErrorClass) => {
+  //нашли поле
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  //убрали класс ошибки и сообщение
   inputElement.classList.remove(inputErrorClass);
   errorElement.textContent = '';
 }
 
+//проверка корректности поля ввода
 const checkInputValidity = (formElement, inputElement, {inputErrorClass}) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, inputErrorClass);
@@ -19,12 +24,14 @@ const checkInputValidity = (formElement, inputElement, {inputErrorClass}) => {
   };
 }
 
+//проверка на наличие невалидных полей. Если хоть одно поле с ошибкой, будет возвращено true
 const hasInvalidInput = (inputElements) => {
   return inputElements.some((inputElement) => {
     return !inputElement.validity.valid;
   });
 }
 
+//если есть поля с ошибками, нужно сделать кнопку сабмита неактивной и добавить соответствующий класс, и наоборот
 const toggleButtonState = (inputElements, buttonElement, inactiveButtonClass) => {
   if (hasInvalidInput(inputElements)) {
     buttonElement.classList.add(inactiveButtonClass);
@@ -37,9 +44,12 @@ const toggleButtonState = (inputElements, buttonElement, inactiveButtonClass) =>
 }
 
 const setEventListeners = (formElement, {inputSelector, submitButtonSelector, inactiveButtonClass, ...rest}) => {
+  //создали массив полей ввода формы
   const inputElements = Array.from(formElement.querySelectorAll(inputSelector));
   const buttonElement = formElement.querySelector(submitButtonSelector);
+  //уже перед первым вводом проверили поля формы на валидность, активировали или деактивировали кнопку сабмита
   toggleButtonState(inputElements, buttonElement, inactiveButtonClass);
+  //на каждое поле повесили слушателя ввода, который отслеживает валидность полей и следит за состоянием кнопки сабмита
   inputElements.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
       checkInputValidity(formElement, inputElement, rest);
@@ -48,6 +58,7 @@ const setEventListeners = (formElement, {inputSelector, submitButtonSelector, in
   });
 }
 
+//нашли все формы в документе и на каждую повесили слушатели ввода
 const enableValidation = ({formSelector, ...rest}) => {
   const formList = Array.from(document.querySelectorAll(formSelector));
   formList.forEach((formElement) => {
@@ -63,5 +74,4 @@ enableValidation({
   submitButtonSelector: '.popup__form-button',
   inactiveButtonClass: 'popup__form-button_disabled',
   inputErrorClass: 'popup__form-input_type_error',
-//  errorClass: 'popup__input-error'
 });
